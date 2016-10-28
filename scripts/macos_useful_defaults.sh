fffe384f2c4bb4c48f5c5af2a721ad429cd9fbca
Richard Ting <richard.tin@gmail.com>
2016-10-29 00:14:49 +0800

    Add macOS useful defaults settings

---
+++ scripts/macos_useful_defaults.sh
@@ -0,0 +1,12 @@
+# Set default UTF-8 encoding for Mail.app
+
+defaults write com.apple.mail NSPreferredMailCharset "UTF-8"
+
+# Show Hidden Files in Mac OS X Finder
+
+defaults write com.apple.finder AppleShowAllFiles true
+
+# Prevent .DS_Store file creation over network connections
+
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
